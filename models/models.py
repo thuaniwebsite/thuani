0ca1c1479bb4444cb2d73b77e39c563226359b04
thuaniwebsite <nhon@thuani.com>
2019-05-06 19:09:08 +0800

    b4 copying from resconfigsettings

--- models/models.py
+++ models/models.py
@@ -6,10 +6,14 @@ class ThuaniDashboard(models.Model):
     _name = "thuani.dashboard"
     _description = "thuani Dashboard"
 
-    # Blog
     name = fields.Char(string="Dashboard", required=False, )
     thuani_blog_url_permalink = fields.Char(string="Blog URL", required=False, )
     thuani_blog_url_category = fields.Char(string="Category", required=False, )
     thuani_blog_url_postname = fields.Char(string="Postname", required=False, )
     website_name = fields.Char(string="Website Name", required=False, )
 
+class ThuaniBlog(models.Model):
+    _name = "thuani.blog"
+    _description = "thuani Blog"
+
+    blog_category = fields.Char(string="Blog Category", required=False, )
