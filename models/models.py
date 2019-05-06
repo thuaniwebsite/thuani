dd33fd1f58c3c47b5fd2895a6afe0df7008c3266
thuaniwebsite <nhon@thuani.com>
2019-05-06 11:28:05 +0800

    weird layout

--- models/models.py
+++ models/models.py
@@ -11,5 +11,5 @@ class ThuaniDashboard(models.Model):
     thuani_blog_url_permalink = fields.Char(string="Blog URL", required=False, )
     thuani_blog_url_category = fields.Char(string="Category", required=False, )
     thuani_blog_url_postname = fields.Char(string="Postname", required=False, )
-
+    website_name = fields.Char(string="Website Name", required=False, )
 
