7efd2c42964760312ff88e74f7045d157fe4cb00
thuaniwebsite <nhon@thuani.com>
2019-05-09 19:15:05 +0800

    blog menu post working

--- models/models.py
+++ models/models.py
@@ -2,18 +2,31 @@
 
 from odoo import models, fields, api
 
-class ThuaniDashboard(models.Model):
-    _name = "thuani.dashboard"
-    _description = "thuani Dashboard"
+class ThuaniPost(models.Model):
+    _name = "thuani.post"
+    _description = "thuani Post"
 
-    name = fields.Char(string="Dashboard", required=False, )
-    thuani_blog_url_permalink = fields.Char(string="Blog URL", required=False, )
-    thuani_blog_url_category = fields.Char(string="Category", required=False, )
-    thuani_blog_url_postname = fields.Char(string="Postname", required=False, )
-    website_name = fields.Char(string="Website Name", required=False, )
+    heading = fields.Char(string="Heading", required=False, )
 
-class ThuaniBlog(models.Model):
-    _name = "thuani.blog"
-    _description = "thuani Blog"
-
-    blog_category = fields.Char(string="Blog Category", required=False, )
+# class ThuaniDashboard(models.Model):
+#     _name = "thuani.dashboard"
+#     _description = "thuani Dashboard"
+#
+#     name = fields.Char(string="Dashboard", required=False, )
+#     thuani_blog_url_permalink = fields.Char(string="Blog URL", required=False, )
+#     thuani_blog_url_category = fields.Char(string="Category", required=False, )
+#     thuani_blog_url_postname = fields.Char(string="Postname", required=False, )
+#     website_name = fields.Char(string="Website Name", required=False, )
+#
+# class ThuaniBlog(models.Model):
+#     _name = "thuani.blog"
+#     _description = "thuani Blog"
+#
+#     blog_category = fields.Char(string="Blog Category", required=False, )
+#
+#
+# class Thuani.Post(models.Model):
+#     _name = "thuani.post"
+#     _description = "thuani Post"
+#
+#     heading = fields.Char(string="Heading", required=False, )
