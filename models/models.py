710b28039fe557fe501d5efc6db4cfb5c0c5c481
thuaniwebsite <nhon@thuani.com>
2019-05-11 13:48:14 +0800

    base url working

--- models/models.py
+++ models/models.py
@@ -2,95 +2,129 @@
 
 from odoo import models, fields, api
 
+
 class ThuaniPost(models.Model):
     _name = "thuani.post"
     _description = "thuani Post"
 
-    heading = fields.Char()
-    heading_url = fields.Char()
-    # default_code = fields.Char()
+    # fields for Heading of the Post (article/blog)
+    heading = fields.Char(string="Heading", required=True, )  # Heading (Title) of the Post/Article
+    heading_url = fields.Char(string="Heading URL", required=False, )  # URL of the post
 
-    # Making heading replace space with dash; make all heading lowercase
+    base = fields.Char(string="Base", required=False, )
+    base_url = fields.Char(string="Base URL", required=False, )
+
+    # Combine URL
+    combine_url = fields.Char(string="Combine URL", required=False, )
+
+    # Making heading replace space with dash; make all heading lowercase. When heading is updated, the url is updated
     @api.onchange('heading')
-    def set_lower(self):
+    def set_heading_url(self):
         # replace space with dash; make everything lowercase .lower()
         self.heading_url = str(self.heading).replace(' ', '-').lower()
+        self.combine_url = str("/" + self.base_url + "/" + self.heading_url)
         return
 
+    @api.onchange('base')
+    def set_base_url(self):
+        # replace space with dash; make everything lowercase .lower()
+        self.base_url = str(self.base)
+        self.combine_url = str("/" + self.base_url + "/" + self.heading_url)
+        return
 
-    # @api.onchange('heading')
-    # def set_upper(self):
-    #     self.heading = str(self.heading).upper()
+    # base url. When base is updated
+    # @api.onchange('base')
+    # def set_lower(self):
+    #     # replace space with dash; make everything lowercase .lower()
+    #     self.base_url = str(self.base).lower()
     #     return
 
+    # # combine url. When base is updated; or heading is updated
+    # @api.onchange('base_url')
+    # def set_lower(self):
+    #     # replace space with dash; make everything lowercase .lower()
+    #     self.combine_url = str(self.base_url+self.heading).lower()
+    #     return
+    # @api.onchange('heading_url')
+    # def set_lower(self):
+    #     # replace space with dash; make everything lowercase .lower()
+    #     self.combine_url = str(self.base_url+self.heading).lower()
+    #     return
 
-    # @api.onchange('heading')
-    # def onchange_case(self, default_code):
-    #     result = {'value': {
-    #         'default_code': str(default_code).lower()
-    #         }
-    #     }
-    #     return result
-
-    # heading = fields.Char(string="Heading", required=False, )
-    # heading_url_lower_copy = fields.Char(string="Heading URL Lower Copy", related='heading')
-    # heading_url = fields.Char(string="Heading URL Lower Copy", related='heading_url_lower_copy')
-    # heading_url[1] = heading_url[1].lower()
-
-    # @api.onchange('heading')
-    # def set_code(self, heading_url):
-    #     if not heading_url:
-    #         return False
-    #     return heading_url.lower()
+# TODO: Check for unique url when all is combined. If not unique at serial number in postname
 
 
-
-    # @api.onchange('heading')
-    # def set_code(self):
-    #     if not heading:
-    #         self.heading_url = self.heading.lower()
-    #     return self
-
-    # def _sanitize_email(self, email):
-    #     """ Sanitize and standardize blacklist entries: all emails should be
-    #     only real email extracted from strings (A <a@a> -> a@a)  and should be
-    #     lower case. """
-    #     emails = tools.email_split(email)
-    #     if not emails or len(emails) != 1:
-    #         return False
-    #     return emails[0].lower()
-
-    # @api.onchange('heading')
-    # def set_code(self):
-    #     self.heading_url = self.heading.lower()
-
-        # if heading_url_lower_copy is not None:
-        #     heading_url_lower = str(heading_url_lower_copy).lower()
-        # return heading_url_lower_copy
-
-    # url_text = fields.Text()
-    # url_text = heading_url_lower_copy
-    # if heading_url_lower_copy is not None:
-    #     heading_url_lower = heading_url_lower_copy(string="Heading URL Lower")
-
-    # heading_url = fields.Char(string="Heading URL", required=False, )
+# @api.onchange('heading')
+# def set_upper(self):
+#     self.heading = str(self.heading).upper()
+#     return
 
 
-    # makes heading_url = heading field
-    # heading_url = heading_url.lower()
+# @api.onchange('heading')
+# def onchange_case(self, default_code):
+#     result = {'value': {
+#         'default_code': str(default_code).lower()
+#         }
+#     }
+#     return result
 
-    # when heading is changed, this gets done. Makes heading url same as heading, and lower case
-    # has problems when creating new heading
-    # @api.onchange('heading')
-    # def set_code(self):
-    #     self.heading_url = self.heading.lower()
+# heading = fields.Char(string="Heading", required=False, )
+# heading_url_lower_copy = fields.Char(string="Heading URL Lower Copy", related='heading')
+# heading_url = fields.Char(string="Heading URL Lower Copy", related='heading_url_lower_copy')
+# heading_url[1] = heading_url[1].lower()
+
+# @api.onchange('heading')
+# def set_code(self, heading_url):
+#     if not heading_url:
+#         return False
+#     return heading_url.lower()
 
 
+# @api.onchange('heading')
+# def set_code(self):
+#     if not heading:
+#         self.heading_url = self.heading.lower()
+#     return self
 
-    #heading_url = heading
+# def _sanitize_email(self, email):
+#     """ Sanitize and standardize blacklist entries: all emails should be
+#     only real email extracted from strings (A <a@a> -> a@a)  and should be
+#     lower case. """
+#     emails = tools.email_split(email)
+#     if not emails or len(emails) != 1:
+#         return False
+#     return emails[0].lower()
 
-    # get heading and turn it into url
-    # fox.lower()       # all lower cases
+# @api.onchange('heading')
+# def set_code(self):
+#     self.heading_url = self.heading.lower()
+
+# if heading_url_lower_copy is not None:
+#     heading_url_lower = str(heading_url_lower_copy).lower()
+# return heading_url_lower_copy
+
+# url_text = fields.Text()
+# url_text = heading_url_lower_copy
+# if heading_url_lower_copy is not None:
+#     heading_url_lower = heading_url_lower_copy(string="Heading URL Lower")
+
+# heading_url = fields.Char(string="Heading URL", required=False, )
+
+
+# makes heading_url = heading field
+# heading_url = heading_url.lower()
+
+# when heading is changed, this gets done. Makes heading url same as heading, and lower case
+# has problems when creating new heading
+# @api.onchange('heading')
+# def set_code(self):
+#     self.heading_url = self.heading.lower()
+
+
+# heading_url = heading
+
+# get heading and turn it into url
+# fox.lower()       # all lower cases
 
 # class ThuaniDashboard(models.Model):
 #     _name = "thuani.dashboard"
