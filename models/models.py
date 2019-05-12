c83f1273a1a20ecaef6b611be209a6d46b2fa439
thuaniwebsite <nhon@thuani.com>
2019-05-12 19:50:56 +0800

    doing checkbox to view category

--- models/models.py
+++ models/models.py
@@ -46,6 +46,7 @@ class ThuaniPost(models.Model):
             self.combine_url = str("/" + self.base_url + "/" + self.heading_url)
         return
 
+
 class ThuaniBase(models.Model):
     _name = "thuani.base"
     _description = "thuani Base"
@@ -62,6 +63,7 @@ class ThuaniPost2(models.Model):
 
     # this takes data from thuani.base in field base (rec name is base)
     base_many = fields.Many2one(comodel_name="thuani.base", string="Base Many", required=False, )
+
 
 # class ThuaniCategoryRank(models.Model):
 #     _name = "thuani.category.rank"
@@ -87,146 +89,29 @@ class ThuaniCategoryCombine(models.Model):
 
     category_combine = fields.Many2one(comodel_name="thuani.category", string="Category Combine", required=False, )
 
+    # @api.multi
+    # def method_name(self):
+    #     view_id = self.env.ref('thuani.category.tree_view_thuani_category').id
+    #     context = self._context.copy()
+    #     return {
+    #         'name': 'form_name',
+    #         'view_type': 'form',
+    #         'view_mode': 'tree',
+    #         'views': [(view_id, 'form')],
+    #         'res_model': 'thuani.category',
+    #         'view_id': view_id,
+    #         'type': 'ir.actions.act_window',
+    #         'res_id': self.id,
+    #         'target': 'new',
+    #         'context': context,
+    #     }
 
-
-
-# working
-    # @api.onchange('base')
-    # def set_base_url(self):
-    #     # replace space with dash; make everything lowercase .lower()
-    #     self.base_url = str(self.base)
-    #     # check if theres data in combine url. If not, make it a string so return has no error bool false not string
-    #     if self.combine_url == False:
-    #         self.combine_url = 'string'
-    #     # if have data, combine the url
-    #     else:
-    #         self.combine_url = str("/" + self.base_url + "/" + self.heading_url)
-    #     return
-    #
-    # # Making heading replace space with dash; make all heading lowercase. When heading is updated, the url is updated
-    # @api.onchange('heading')
-    # def set_heading_url(self):
-    #     # replace space with dash; make everything lowercase .lower()
-    #     self.heading_url = str(self.heading).replace(' ', '-').lower()
-    #     self.combine_url = str("/" + self.base_url + "/" + self.heading_url)
-    #     return
-
-
-
-
-    # base url. When base is updated
-    # @api.onchange('base')
-    # def set_lower(self):
-    #     # replace space with dash; make everything lowercase .lower()
-    #     self.base_url = str(self.base).lower()
-    #     return
-
-    # # combine url. When base is updated; or heading is updated
-    # @api.onchange('base_url')
-    # def set_lower(self):
-    #     # replace space with dash; make everything lowercase .lower()
-    #     self.combine_url = str(self.base_url+self.heading).lower()
-    #     return
-    # @api.onchange('heading_url')
-    # def set_lower(self):
-    #     # replace space with dash; make everything lowercase .lower()
-    #     self.combine_url = str(self.base_url+self.heading).lower()
-    #     return
 
 # TODO: Check for unique url when all is combined. If not unique at serial number in postname
 
-
-# @api.onchange('heading')
-# def set_upper(self):
-#     self.heading = str(self.heading).upper()
-#     return
-
-
-# @api.onchange('heading')
-# def onchange_case(self, default_code):
-#     result = {'value': {
-#         'default_code': str(default_code).lower()
-#         }
-#     }
-#     return result
-
-# heading = fields.Char(string="Heading", required=False, )
-# heading_url_lower_copy = fields.Char(string="Heading URL Lower Copy", related='heading')
-# heading_url = fields.Char(string="Heading URL Lower Copy", related='heading_url_lower_copy')
-# heading_url[1] = heading_url[1].lower()
-
-# @api.onchange('heading')
-# def set_code(self, heading_url):
-#     if not heading_url:
-#         return False
-#     return heading_url.lower()
-
-
-# @api.onchange('heading')
-# def set_code(self):
-#     if not heading:
-#         self.heading_url = self.heading.lower()
-#     return self
-
-# def _sanitize_email(self, email):
-#     """ Sanitize and standardize blacklist entries: all emails should be
-#     only real email extracted from strings (A <a@a> -> a@a)  and should be
-#     lower case. """
-#     emails = tools.email_split(email)
-#     if not emails or len(emails) != 1:
-#         return False
-#     return emails[0].lower()
-
-# @api.onchange('heading')
-# def set_code(self):
-#     self.heading_url = self.heading.lower()
-
-# if heading_url_lower_copy is not None:
-#     heading_url_lower = str(heading_url_lower_copy).lower()
-# return heading_url_lower_copy
-
-# url_text = fields.Text()
-# url_text = heading_url_lower_copy
-# if heading_url_lower_copy is not None:
-#     heading_url_lower = heading_url_lower_copy(string="Heading URL Lower")
-
-# heading_url = fields.Char(string="Heading URL", required=False, )
-
-
-# makes heading_url = heading field
-# heading_url = heading_url.lower()
-
-# when heading is changed, this gets done. Makes heading url same as heading, and lower case
-# has problems when creating new heading
-# @api.onchange('heading')
-# def set_code(self):
-#     self.heading_url = self.heading.lower()
-
-
-# heading_url = heading
-
-# get heading and turn it into url
-# fox.lower()       # all lower cases
-
-# class ThuaniDashboard(models.Model):
-#     _name = "thuani.dashboard"
-#     _description = "thuani Dashboard"
-#
-#     name = fields.Char(string="Dashboard", required=False, )
-#     thuani_blog_url_permalink = fields.Char(string="Blog URL", required=False, )
-#     thuani_blog_url_category = fields.Char(string="Category", required=False, )
-#     thuani_blog_url_postname = fields.Char(string="Postname", required=False, )
-#     website_name = fields.Char(string="Website Name", required=False, )
-#
-# class ThuaniBlog(models.Model):
-#     _name = "thuani.blog"
-#     _description = "thuani Blog"
-#
-#     blog_category = fields.Char(string="Blog Category", required=False, )
-#
-#
-# class Thuani.Post(models.Model):
-#     _name = "thuani.post"
-#     _description = "thuani Post"
-#
-#     heading = fields.Char(string="Heading", required=False, )
+# Create new menu:
+# models/models.py make new class >
+# views/xmlname.xml create odooform; odootree; odooaction; menu >
+# security/ir.model.access.csv add new line >
+# access_thuani_category_combine,thuani.thuani,model_thuani_category_combine,,1,1,1,1
+# access_uniqueNameOfClass; model_ is _name in your class
